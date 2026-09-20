b033434033cfe3b0a3a6066052df233f6dba3fa2
yusufberk-k <239020644+yusufberk-k@users.noreply.github.com>
2026-09-20 22:07:50 +0300

    Add 271. Encode and Decode Strings (length-prefix encoding, O(n))

---
+++ arrays-hashing/encode-and-decode-strings.py
@@ -0,0 +1,25 @@
+# Encode and Decode Strings
+# status: looked | retry: 2026-09-22
+# note: did not know the length-prefix pattern
+# switched from C++ to Python here - C++ made this more about buffers
+# than about the algorithm
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for word in strs:
+            res += str(len(word)) + "!" + word
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "!":
+                j += 1
+            length = int(s[i:j])
+            res.append(s[j+1 : j+1+length])
+            i = j + 1 + length
+        return res
